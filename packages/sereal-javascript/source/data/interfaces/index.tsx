12dbd1feba531f34a1ad3b0a3c6e15a8951b96d0
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-04-11 23:31:20 +0300

    sereal-javascript > dev: source

--- packages/sereal-javascript/source/data/interfaces/index.tsx
+++ packages/sereal-javascript/source/data/interfaces/index.tsx
@@ -1,3 +1,11 @@
 // #region module
-export {};
+export interface SerealableObject<S> {
+    toSereal(): S;
+    loadSereal(state: S): void;
+}
+
+export interface SerealObjectStored<C> {
+    class: C;
+    current: SerealableObject<C>;
+}
 // #endregion module
--- packages/sereal-javascript/source/objects/Sereal/index.ts
+++ packages/sereal-javascript/source/objects/Sereal/index.ts
@@ -1,6 +1,25 @@
 // #region methods
-class Sereal {
+class Sereal<S = any> {
+    private state: S | undefined;
 
+
+    public extract() {
+        if (!this.state) {
+            return;
+        }
+
+        const serialization = {};
+
+        for (const [key, value] of Object.entries(this.state)) {
+            serialization[key] = value;
+        }
+    }
+
+    public load(
+        state: any,
+    ) {
+
+    }
 }
 // #endregion methods
 
