aecf25aa4db2c08f8b86a227a88604cf45ea2d7d
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-04-12 23:15:13 +0300

    sereal-javascript > dev: Sereal

--- packages/sereal-javascript/source/objects/Sereal/__tests__/index.test.ts
+++ packages/sereal-javascript/source/objects/Sereal/__tests__/index.test.ts
@@ -1,5 +1,9 @@
 // #region imports
     // #region external
+    import {
+        SerealableObject,
+    } from '../../../data/interfaces';
+
     import Sereal from '../';
     // #endregion external
 // #endregion imports
@@ -9,6 +13,28 @@
 // #region module
 describe('Sereal', () => {
     it('works', () => {
+        class SomeSereal implements SerealableObject<any> {
+            private value = 23;
+
+            public increase() {
+                this.value += 1;
+            }
+
+            public toSereal() {
+                return {
+                    value: this.value,
+                };
+            }
+
+            public loadSereal(
+                state: any,
+            ) {
+                this.value = state.value;
+            }
+        }
+
+        const someSereal = new SomeSereal();
+
         const sereal = new Sereal({
             one: '1',
         });
@@ -16,6 +42,21 @@ describe('Sereal', () => {
         sereal.step({
             one: '2',
         });
+        console.log(sereal.extract());
+
+        sereal.step({
+            someSereal: {
+                class: SomeSereal,
+                current: someSereal,
+            },
+        });
+
+        console.log(sereal.extract());
+
+        someSereal.increase();
+        someSereal.increase();
+
+        console.log(sereal.extract());
 
         expect(true).toBeTruthy();
     });
